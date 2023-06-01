ea062656ed84453b4d32018be6bfbd92e72bcf2c
Jon Farhat <Farufon@users.noreply.github.com>
2023-05-31 20:46:44 -0700

    2

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'Proportional Spray Cap'}
+        subtitle={'Turn Any Spray Can Into an Airbrush'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
